cbaa8f9ef8d95676e26431810ee7cad9af2c0278
Arash Altafi <arashaltafi1377@gmail.com>
2023-04-07 16:54:27 +0430

    Commit Add Sample 4

--- app/src/main/java/com/arash/altafi/pdf/pdf4/Pdf4Activity.kt
+++ app/src/main/java/com/arash/altafi/pdf/pdf4/Pdf4Activity.kt
@@ -2,13 +2,23 @@ package com.arash.altafi.pdf.pdf4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
 import com.arash.altafi.pdf.databinding.ActivityPdf4Binding
+import es.voghdev.pdfviewpager.library.RemotePDFViewPager
+import es.voghdev.pdfviewpager.library.adapter.PDFPagerAdapter
+import es.voghdev.pdfviewpager.library.remote.DownloadFile
+import es.voghdev.pdfviewpager.library.util.FileUtil
 
-class Pdf4Activity : AppCompatActivity() {
+class Pdf4Activity : AppCompatActivity(), DownloadFile.Listener {
 
     private val binding by lazy {
         ActivityPdf4Binding.inflate(layoutInflater)
     }
+    private val pdfUrl = "https://arashaltafi.ir/url_sample/pdf.pdf"
+    private lateinit var remotePdf: RemotePDFViewPager
+    private lateinit var adapter: PDFPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +28,40 @@ class Pdf4Activity : AppCompatActivity() {
     }
 
     private fun init() = binding.apply {
+        val listener: DownloadFile.Listener = this@Pdf4Activity
 
+        btnDownload.setOnClickListener {
+            remotePdf = RemotePDFViewPager(this@Pdf4Activity, pdfUrl, listener)
+            progressbar.visibility = View.VISIBLE
+        }
+    }
+
+    private fun updateLayout() = binding.apply {
+        progressbar.visibility = View.GONE
+        root.removeAllViewsInLayout()
+        root.addView(
+            btnDownload,
+            LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        root.addView(
+            remotePdf,
+            LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+    }
+
+    override fun onSuccess(url: String?, destinationPath: String?) {
+        Toast.makeText(applicationContext, "Download Success!", Toast.LENGTH_SHORT).show()
+        adapter = PDFPagerAdapter(this, FileUtil.extractFileNameFromURL(url))
+        remotePdf.adapter = adapter
+        updateLayout()
+    }
+
+    override fun onFailure(e: Exception?) {
+        e?.printStackTrace()
+        binding.progressbar.visibility = View.GONE
+    }
+
+    override fun onProgressUpdate(progress: Int, total: Int) {
     }
 
 }
