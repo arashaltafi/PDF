e14405d0ddda8bd975a4cdf2c9bb8fda48a5c441
Arash Altafi <arashaltafi1377@gmail.com>
2023-04-13 01:34:27 +0430

    Commit Add Sample 5

--- app/src/main/java/com/arash/altafi/pdf/pdf5/Pdf5Activity.kt
+++ app/src/main/java/com/arash/altafi/pdf/pdf5/Pdf5Activity.kt
@@ -1,9 +1,13 @@
 package com.arash.altafi.pdf.pdf5
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.arash.altafi.pdf.R
 import com.arash.altafi.pdf.databinding.ActivityPdf5Binding
+import com.pspdfkit.configuration.activity.PdfActivityConfiguration
+import com.pspdfkit.ui.PdfActivity
+import com.pspdfkit.ui.PdfActivityIntentBuilder
 
 class Pdf5Activity : AppCompatActivity() {
 
@@ -19,7 +23,12 @@ class Pdf5Activity : AppCompatActivity() {
     }
 
     private fun init() = binding.apply {
+        val uri = Uri.parse("file:///android_asset/pdf.pdf")
+//        val config = PdfActivityConfiguration.Builder(this@Pdf5Activity).build()
+//        PdfActivity.showDocument(this@Pdf5Activity, uri, config)
 
+        val intent = PdfActivityIntentBuilder.fromUri(this@Pdf5Activity, uri).build()
+        startActivity(intent)
     }
 
 }
